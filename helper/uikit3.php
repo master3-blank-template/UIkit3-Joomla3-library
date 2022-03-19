6e013ca70b7e37a1e250ea69bb2287522234a150
AlekVolsk <alekvolsk@yandex.ru>
2022-03-20 01:45:05 +0400

    v3.13.1

--- helper/uikit3.php
+++ helper/uikit3.php
@@ -1,10 +1,13 @@
-<?php defined('JPATH_PLATFORM') or die;
+<?php
+
 /**
  * @package     Joomla.Libraries
  * @subpackage  HTML
  * @copyright   Copyright (C) Aleksey A. Morozov. All rights reserved.
  * @license     GNU General Public License version 3 or later; see http://www.gnu.org/licenses/gpl-3.0.txt
  */
+
+\defined('JPATH_PLATFORM') or die;
 
 use Joomla\CMS\HTML\HTMLHelper;
 
